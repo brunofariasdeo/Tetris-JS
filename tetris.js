c6b96dee9cd82db56e5833a789fabcafd5414078
diegodnz <diegodiniz991@gmail.com>
2020-09-22 03:08:26 -0300

    Added query selectors

--- tetris.js
+++ tetris.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded'), () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const width = 10
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const score = document.querySelector('#score')
+    const startBt = document.querySelector('#start-button')
 
-}
+})
+
+
