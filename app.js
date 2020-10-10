fb6a6c74e654cb7229fa87ea3a79655c6044a186
Bruno Oliveira <bruno.oliveira@liferay.com>
2020-10-10 14:57:34 -0300

    Create node server

---
+++ app.js
@@ -0,0 +1,16 @@
+// create an express app
+const express = require("express")
+const app = express()
+const path = require("path");
+// use the express-static middleware
+app.use(express.static("src"))
+
+// define the first route
+app.get("/", function (req, res) {
+    // res.send("<h1>Hello World!</h1>")
+    res.sendFile(path.join(__dirname,'./src/index.html'));
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
